04e87b141dd3b43d5afcd035131be12e6f362d4e
EthanHung0 <aaronoriginal2009@gmail.com>
2025-09-25 21:42:16 +0700

    e

---
+++ ReWrite.py
@@ -0,0 +1,385 @@
+import pygame
+from pygame.locals import *
+import sys
+import random
+
+
+
+pygame.init()
+
+info = pygame.display.Info() # 1536 864
+WIDTH, HEIGHT = info.current_w, info.current_h - 50 # 1536 814
+DISPLAY_SURFACE = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+
+background = pygame.image.load("bg.png").convert()
+background = pygame.transform.smoothscale(background, (WIDTH, HEIGHT))
+
+pygame.display.set_caption('Horse Racing')
+clock = pygame.time.Clock()
+font = pygame.font.Font("DejaVuSans.ttf", 25)
+alt_font = pygame.font.Font("KoushikiSans/KoushikiSans-Regular.ttf", 30)
+
+GAME_TIME = 600000
+
+finish_line = WIDTH - 150
+race_length = finish_line - 150
+
+# ================================================================================
+# Utilities
+
+def scale(img, base_scale):
+    width, height = img.get_size()
+    scale = base_scale / max(width, height)
+    new_size = (int(width * scale), int(height * scale))
+    return pygame.transform.smoothscale(img, new_size)
+
+
+def generateSteps():
+    step_weights = [random.uniform(0.5, 1.5) for _ in range(10)]
+    total_weights = sum(step_weights)
+    plan = [int(weight / total_weights * race_length) for weight in step_weights]
+
+    diff = race_length - sum(plan)
+    for i in range(diff):
+        plan[i % 10] += 1
+
+    plan[-1] = race_length - sum(plan[:-1])
+    return plan
+
+
+# ================================================================================
+# Horse Class
+
+
+class Horse:
+    def __init__(self, image, y_pos, team):
+        self.team = team
+        self.image = scale(image,100)
+        self.x = 100
+        self.y = y_pos
+        self.steps = generateSteps()
+        self.index = 0
+        self.finished = False
+
+
+    def move(self) -> None:
+        if self.finished:
+            return
+
+        self.x += self.steps[self.index]
+        self.index += 1
+
+        if self.index >= len(self.steps):
+            self.finished = True
+
+
+    def draw(self) -> None:
+        # draw track line
+        pygame.draw.line(
+            DISPLAY_SURFACE,
+            (100, 100, 100),
+            (100, self.y + 50),
+            (finish_line + 50, self.y + 50)
+            )
+        # draw horse
+        DISPLAY_SURFACE.blit(self.image, (self.x, self.y))
+
+# ================================================================================
+# Game Class
+
+class Game:
+    questions = [
+    {
+        "q": "Con vật nào là loài động vật lớn nhất trên Trái Đất?",
+        "answer": "A. Cá voi xanh",
+        "options": ["A. Cá voi xanh", "B. Voi", "C. Cá mập", "D. Gấu bắc cực"]
+    },
+    {
+        "q": "Quốc gia nào có diện tích lớn nhất thế giới?",
+        "answer": "C. Nga",
+        "options": ["A. Hoa Kỳ", "B. Trung Quốc", "C. Nga", "D. Canada"]
+    },
+    {
+        "q": "Thủ đô của Pháp là gì?",
+        "answer": "B. Paris",
+        "options": ["A. London", "B. Paris", "C. Berlin", "D. Rome"]
+    },
+    {
+        "q": "Quốc gia nào là nơi sản xuất cà phê lớn nhất thế giới?",
+        "answer": "A. Brazil",
+        "options": ["A. Brazil", "B. Colombia", "C. Việt Nam", "D. Ấn Độ"]
+    },
+    {
+        "q": "Bộ môn thể thao nào được coi là 'vua của các môn thể thao'?",
+        "answer": "A. Bóng đá",
+        "options": ["A. Bóng đá", "B. Bóng rổ", "C. Quần vợt", "D. Cầu lông"]
+    },
+    {
+        "q": "Nước nào được biết tới là có tượng nữ thần tự do?",
+        "answer": "C. New York",
+        "options": ["A. Paris", "B. London", "C. New York", "D. Tokyo"]
+    },
+    {
+        "q": "Ai ghi bàn nhiều nhất lịch sử bóng đá?",
+        "answer": "A. Cristiano Ronaldo",
+        "options": ["A. Cristiano Ronaldo", "B. Lionel Messi", "C. Josef Bican", "D. Romário"]
+    },
+    {
+        "q": "Nam Cao là tác giả của tác phẩm nào đã được học trong sách Ngữ văn 11?",
+        "answer": "C. Chí Phèo",
+        "options": ["A. Lão Hạc", "B. Vợ nhặt", "C. Chí Phèo", "D. Đời thừa"]
+    },
+    {
+        "q": "Quê của nhà văn Kim Lân là tỉnh nào?",
+        "answer": "A. Bắc Ninh",
+        "options": ["A. Bắc Ninh", "B. Bắc Giang", "C. Hà Nội", "D. Hải Dương"]
+    },
+    {
+        "q": "Bức tường Berlin dùng để làm gì?",
+        "answer": "B. Ngăn cách Đông và Tây Đức",
+        "options": ["A. Bảo vệ người dân Đức", "B. Ngăn cách Đông và Tây Đức", "C. Ngăn quân Pháp xâm lược", "D. Là biểu tượng hòa bình"]
+    },
+    {
+        "q": "Bức tranh 'Đêm đầy sao' do ai vẽ?",
+        "answer": "C. Vincent van Gogh",
+        "options": ["A. Leonardo da Vinci", "B. Pablo Picasso", "C. Vincent van Gogh", "D. Claude Monet"]
+    },
+    {
+        "q": "Cơ quan nào có chức năng lọc máu?",
+        "answer": "D. Thận",
+        "options": ["A. Tim", "B. Phổi", "C. Gan", "D. Thận"]
+    },
+    {
+        "q": "Cơ quan nào có chức năng tống máu đi khắp cơ thể?",
+        "answer": "B. Tim",
+        "options": ["A. Gan", "B. Tim", "C. Não", "D. Thận"]
+    },
+    {
+        "q": "Loài động vật nào đứng để ngủ?",
+        "answer": "B. Ngựa",
+        "options": ["A. Bò", "B. Ngựa", "C. Voi", "D. Hươu cao cổ"]
+    },
+    {
+        "q": "Ngày Nhà giáo Việt Nam là ngày nào?",
+        "answer": "B. 20/11",
+        "options": ["A. 20/10", "B. 20/11", "C. 26/3", "D. 2/9"]
+    },
+    {
+        "q": "Ngày 20/10 là ngày gì?",
+        "answer": "B. Ngày Phụ nữ Việt Nam",
+        "options": ["A. Ngày Giải phóng miền Nam", "B. Ngày Phụ nữ Việt Nam", "C. Ngày Quốc tế Lao động", "D. Ngày Quốc tế Phụ nữ"]
+    }
+]
+
+    # --------------------------------------------------------------------------------
+    # initializing
+    def __init__(self):
+        self._horses:list[Horse] = [Horse(pygame.image.load(f"hors{i+1}.png").convert_alpha(), 320 + 100 * i, h) for i,h in enumerate(self._getTeamNames(4),0)]
+        self._winner:list[Horse] | Horse = None
+        self._frozen_time = 0
+        self._banner_pos = WIDTH
+        self._current_question = None
+
+    @property
+    def horses(self) -> list[Horse]:
+        return self._horses
+
+    @property
+    def winner(self) -> Horse | list[Horse]:
+        return self._winner
+    @winner.setter
+    def winner(self,horse) -> None:
+        self._winner = horse
+
+    @property
+    def frozen_time(self) -> int:
+        return self._frozen_time
+    @frozen_time.setter
+    def frozen_time(self,val) -> None:
+        self._frozen_time = val
+
+
+    # --------------------------------------------------------------------------------
+    # Ultility method
+    def _getTeamNames(self, num:int) -> list[str]:
+        names = []
+        current = ""
+        while len(names) < num:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_RETURN:
+                        if current.strip():
+                            names.append(current.strip())
+                            current = ""
+                    elif event.key == K_BACKSPACE:
+                        current = current[:-1]
+                    else:
+                        current += event.unicode
+
+        # draw input screen
+            DISPLAY_SURFACE.fill((0, 0, 0))
+            prompt = alt_font.render(f"Input Horse's team {len(names) + 1 if len(names) < 4 else 4}:", True, (255, 255, 255))
+            input_text = alt_font.render(current, True, (255, 215, 0))
+            DISPLAY_SURFACE.blit(prompt, (100, HEIGHT // 2 - 40))
+            DISPLAY_SURFACE.blit(input_text, (100, HEIGHT // 2))
+            pygame.display.flip()
+            clock.tick(30)
+        return names
+
+
+    def resetQuestion(self) -> None:
+        if self._current_question in self.questions:
+            self.questions.remove(self._current_question)
+
+    # --------------------------------------------------------------------------------
+    # drawing methods
+    def drawTimer(self) -> None:
+        now = pygame.time.get_ticks()
+        if not self._winner:
+            main_timer = max(0, GAME_TIME - now)
+        else:
+            main_timer = max(0, GAME_TIME - (self.frozen_time if self.frozen_time else now)) # miliseconds
+
+        if not self._winner:
+            if not self.questions or now >= GAME_TIME:
+                self.frozen_time = now
+                highest_val = max(h.x for h in self._horses)
+                self._winners = [h for h in self._horses if h.x == highest_val]
+                self._winner = self._winners if len(self._winners) > 1 else self._winners[0]
+
+        main_min = main_timer // 60000
+        main_sec = (main_timer % 60000) // 1000
+
+        timer_txt = alt_font.render(f"{main_min:02}:{main_sec:02}", True, (255,255,255))
+        DISPLAY_SURFACE.blit(timer_txt, (WIDTH-230, 37))
+
+
+    def drawWinningBanner(self) -> None:
+        img = pygame.image.load("tie.jpg" if isinstance(self._winner,list) else "1st.jpg").convert_alpha()
+        img = scale(img,200)
+        img.set_colorkey((0,0,0))
+
+        banner = pygame.Surface((WIDTH,300), SRCALPHA)
+        banner.fill((0,0,0,180))
+
+        text = ""
+        name_list = []
+        if isinstance(self._winner,list):
+            for horse in self._winner:
+                name_list.append(horse.team)
+            text = ", ".join(name_list) + " Tied!"
+        else:
+            text = f"{self._winner.team} Wins!"
+
+        text = alt_font.render(text, True, (255, 215, 0))
+        text_rect = text.get_rect(center= (WIDTH//2, 150))
+        img_rect = img.get_rect(center= (WIDTH//2, 150))
+        text_rect.y += 100
+        img_rect.y -= 30
+
+        banner.blit(img,img_rect)
+        banner.blit(text,text_rect)
+
+        DISPLAY_SURFACE.blit(banner, (self._banner_pos, HEIGHT//2 - 150))
+        self._banner_pos = max(0,self._banner_pos - 20)
+
+
+    def drawQuestion(self) -> None:
+        if self.questions and not self.winner:
+            if self._current_question not in self.questions:
+                self._current_question = random.choice(self.questions)
+
+            qdata = self._current_question  # store the dict for easier access
+            question_txt = font.render(qdata["q"], True, (255,255,255))
+            DISPLAY_SURFACE.blit(question_txt, (200, 70))
+
+            for i, option in enumerate(qdata["options"]):
+                option_txt = font.render(option, True, (255,255,255))
+                col = i % 2
+                row = i // 2
+                DISPLAY_SURFACE.blit(option_txt,(220 + col*300, 120 + row*50))
+
+
+    def drawHorses(self) -> None:
+        for horse in self.horses:
+            horse.draw()
+
+
+# ================================================================================
+# Setup
+
+GameSystem = Game()
+running = True
+current_question = None
+
+# ================================================================================
+# # Main Loop
+
+while running: #main loop
+    DISPLAY_SURFACE.blit(background,(0,0))
+
+    GameSystem.drawHorses()
+    GameSystem.drawTimer()
+    GameSystem.drawQuestion()
+
+    if GameSystem._winner: # display OUT OF TIME/OUT OF QUESTION if either conditions are met
+        alt_message = "OUT OF TIME" if pygame.time.get_ticks() > GAME_TIME else ("OUT OF QUESTIONS" if not GameSystem.questions else None)
+        if alt_message:
+            alt_wintext = alt_font.render(alt_message, True, (255,0,0))
+            DISPLAY_SURFACE.blit(alt_wintext, (250, 150))
+
+    for event in pygame.event.get():
+        if event.type == QUIT: # X check
+            running = False
+
+        #-----------------------------------------------------------------------------
+        elif event.type == pygame.VIDEORESIZE:
+            # update width and height
+            WIDTH, HEIGHT = event.w, event.h
+            DISPLAY_SURFACE = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+
+            # rescale background
+            background = pygame.transform.smoothscale(
+                pygame.image.load("bg.png").convert(),
+                (WIDTH, HEIGHT)
+            )
+        #-----------------------------------------------------------------------------
+
+        if not GameSystem.winner and GameSystem.questions and pygame.time.get_ticks() < GAME_TIME:
+            if event.type == KEYDOWN:
+                # skipping question
+                if event.key == K_SPACE:
+                    GameSystem.resetQuestion()
+
+                # assigning move keys
+                mapping = {
+                    K_1: GameSystem.horses[0],
+                    K_2: GameSystem.horses[1],
+                    K_3: GameSystem.horses[2],
+                    K_4: GameSystem.horses[3],
+                }
+                horse = mapping.get(event.key)
+
+                # move
+                if horse and not horse.finished:
+                    horse.move()
+                    GameSystem.resetQuestion()
+
+                    # if winner is determined -> stops timer, move horse past finish line
+                    if horse.finished:
+                        frozen_time = pygame.time.get_ticks()
+                        horse.x = finish_line
+                        GameSystem.winner = horse
+    if GameSystem._winner: # draws a winning banner for the winner
+        GameSystem.drawWinningBanner()
+
+    pygame.display.flip()
+    clock.tick(60) # 60 FPS
+
+
+# ================================================================================
+pygame.quit()
+sys.exit()
